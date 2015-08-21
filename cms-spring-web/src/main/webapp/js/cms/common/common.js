d1ff286d9e18729ff9f9330b0beda49cbefe58a8
zhanghanlin <andyzhanghanlin@163.com>
2015-08-21 12:04:57 +0800

    update page info

--- cms-spring-web/src/main/webapp/js/cms/common/common.js
+++ cms-spring-web/src/main/webapp/js/cms/common/common.js
@@ -7,7 +7,7 @@ $(function() {
 							'/menu/tree',
 							function(obj) {
 								if (obj.hasChild) {
-									var html = '';
+									var str = [];
 									$
 											.each(
 													obj.children,
@@ -19,52 +19,68 @@ $(function() {
 														if (breadcrumb[0] == o.node.name) {
 															cls += ' active';
 														}
-														html += '<li class="'
-																+ cls
-																+ '">\
-										<a href="###">\
-											<i class="fa '
-																+ o.node.icon
-																+ '"></i>\
-											<span>'
-																+ o.node.name
-																+ '</span>\
-											<i class="fa fa-angle-left pull-right"></i>\
-										</a>';
-														html += common
-																.subLeftTree(o,
-																		breadcrumb);
+														str.push('<li class="');
+														str.push(cls);
+														str
+																.push('"><a href="###"><i class="fa ');
+														str.push(o.node.icon);
+														str
+																.push('"></i><span>')
+														str.push(o.node.name);
+														str
+																.push('</span><i class="fa fa-angle-left pull-right"></i></a>');
+														str
+																.push(common
+																		.subLeftTree(
+																				o,
+																				breadcrumb));
 													});
-									$('.sidebar-menu').append(html);
+									$('.sidebar-menu').append(str.join(''));
 								}
 							});
 		},
 		subLeftTree : function(obj, breadcrumb) {
-			var html = '';
+			var str = [];
 			if (obj.hasChild) {
 				var cls = '';
+				var style = 'display : none;';
 				if (breadcrumb && breadcrumb[obj.level - 1] == obj.node.name) {
-					cls = ' menu-open';
+					cls = 'menu-open';
+					style = 'display : block;';
 				}
-				html += '<ul class="treeview-menu ' + cls + '">';
-				$.each(obj.children, function(j, o) {
-					var subCls = '';
-					if (breadcrumb && breadcrumb[o.level - 1] == o.node.name) {
-						subCls = 'active';
-					}
-					html += '<li class="' + subCls + '"><a href="'
-							+ o.node.link + '"><i class="fa ' + o.node.icon
-							+ '"></i>' + o.node.name;
-					if (o.children.length > 0) {
-						html += '<i class="fa fa-angle-left pull-right"></i>';
-					}
-					html += '</a>';
-					html += common.subLeftTree(o);
-					html += '</li>';
-				});
-				html += '</ul>';
+				str.push('<ul class="treeview-menu ');
+				str.push(cls);
+				str.push(' " style="');
+				str.push(style)
+				str.push('">');
+				$
+						.each(
+								obj.children,
+								function(j, o) {
+									var subCls = '';
+									if (breadcrumb
+											&& breadcrumb[o.level - 1] == o.node.name) {
+										subCls = 'active';
+									}
+									str.push('<li class="');
+									str.push(subCls);
+									str.push('"><a href="');
+									str.push(o.node.link);
+									str.push('"><i class="fa ');
+									str.push(o.node.icon);
+									str.push('"></i>');
+									str.push(o.node.name);
+									if (o.children.length > 0) {
+										str
+												.push('<i class="fa fa-angle-left pull-right"></i>');
+									}
+									str.push('</a>');
+									str.push(common.subLeftTree(o));
+									str.push('</li>');
+								});
+				str.push('</ul>');
 			}
-			return html;
+			return str.join('');
 		},
 		pageBreadcrumb : function() {
 			return $('.content-header ol li').map(function() {
