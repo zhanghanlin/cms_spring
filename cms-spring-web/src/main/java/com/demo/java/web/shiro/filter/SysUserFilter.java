497a031a16c7ed644deac6c709f5f635cdeea396
zhanghanlin <andyzhanghanlin@163.com>
2015-09-01 18:34:51 +0800

    add shiro

--- cms-spring-web/src/main/java/com/demo/java/web/shiro/filter/SysUserFilter.java
+++ cms-spring-web/src/main/java/com/demo/java/web/shiro/filter/SysUserFilter.java
@@ -5,8 +5,10 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.filter.PathMatchingFilter;
 
+import com.demo.java.user.entity.User;
 import com.demo.java.user.service.UserService;
 import com.demo.java.utils.Constants;
 
@@ -17,8 +19,16 @@ public class SysUserFilter extends PathMatchingFilter {
 
     @Override
     protected boolean preHandle(ServletRequest request, ServletResponse response) throws Exception {
-        String username = (String) SecurityUtils.getSubject().getPrincipal();
-        request.setAttribute(Constants.CURRENT_USER_KEY, userService.findByLogin(username));
+        Subject subject = SecurityUtils.getSubject();
+        if (subject == null) {
+            return false;
+        }
+        Object sessionUser = subject.getSession().getAttribute(Constants.CURRENT_USER_KEY);
+        if (sessionUser == null) {
+            String username = subject.getPrincipal().toString();
+            User user = userService.findByLogin(username);
+            subject.getSession().setAttribute(Constants.CURRENT_USER_KEY, user);
+        }
         return true;
     }
 }
